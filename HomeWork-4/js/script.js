235c14b1e950897b02a925c1646f580d86505242
Roman Yurko <yurko.rom@gmail.com>
2024-06-19 23:23:26 +0300

    upload HW#4

---
+++ HomeWork-4/js/script.js
@@ -0,0 +1,51 @@
+// ---1---
+// Перепишіть функцію, використовуючи '?' або '||'
+
+// function checkAge(age) {
+//   if (age > 18) {
+//     return true;
+//   } else {
+//     return confirm("Батьки дозволили?");
+//   }
+// }
+
+// function checkAge(age) {
+//     age > 18 ? true : confirm("Батьки дозволили?")
+//     }
+
+// ---2---
+// Напишіть функцію min(a, b), яка повертає менше з двох чисел a та b.
+
+// function min(a, b) {
+//   if (a === b) console.log("вони рівні");
+//   else if (a < b) console.log("min: ", a);
+//   else console.log("min: ", b);
+// }
+
+// ---3---
+// Перепишіть з використанням стрілкових функцій
+// Замініть Функціональні Вирази на стрілкові функції у коді нижче:
+
+// function ask(question, yes, no) {
+//   if (confirm(question)) yes();
+//   else no();
+// }
+// ask(
+//   "Ви згодні?",
+//   function () {
+//     alert("Ви погодились.");
+//   },
+//   function () {
+//     alert("Ви скасували виконання.");
+//   }
+// );
+
+// function ask(question, yes, no) {
+//   if (confirm(question)) yes();
+//   else no();
+// }
+// ask(
+//   "Ви згодні?",
+//   () => alert("Ви погодились."),
+//   () => alert("Ви скасували виконання.")
+// );
