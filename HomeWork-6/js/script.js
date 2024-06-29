48b44116ba6d2e849d13a668b5b6dcbeb886a333
Roman Yurko <yurko.rom@gmail.com>
2024-06-29 15:29:09 +0300

    upload HW#6

---
+++ HomeWork-6/js/script.js
@@ -0,0 +1,47 @@
+// Створити телефонну книгу
+// -створити початковий клас Abonent, де зберігатимуться ім*я і номер
+// -створити set який прийматиме телефон і номер
+// -створити get який виводитиме данні про абонента
+// -створити три різних юзери
+// -вивести данні
+
+// class Abonent {
+//   constructor(name, phone) {
+//     this.name = name.name;
+//     this.phoneNumber = name.phoneNumber;
+//   }
+//   firstName = "";
+//   lastName = "";
+//   phoneNumber = "";
+
+//   set name(newName) {
+//     const nameRow = newName.split(" ");
+//     this.firstName = nameRow[0];
+//     this.lastName = nameRow[1];
+//     this.phoneNumber = Number;
+//   }
+
+//   get name() {
+//     return ` first name : ${this.firstName}, last name : ${this.lastName}, phone number : ${this.phoneNumber} `;
+//   }
+// }
+
+// const Anna = new Abonent({
+//   name: "Anna Rock",
+//   phoneNumber: +380987654321,
+// });
+
+// const Inna = new Abonent({
+//   name: "Inna Stone",
+//   phoneNumber: +380123456789,
+// });
+
+// const Serg = new Abonent({
+//   name: "Serg Stus",
+//   phoneNumber: +380192837465,
+// });
+
+// console.group("PhoneBook");
+// console.log(Anna);
+// console.log(Inna);
+// console.log(Serg);
